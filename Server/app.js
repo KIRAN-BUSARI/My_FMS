7d4c160d23de1457eb19a71d388bd3057d8b43b2
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-09-30 23:50:30 +0530

    Initialzed the app.js with / route

---
+++ Server/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+
+config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use('/', (req, res) => {
+    res.send('<h1>This is FMS project</h1>')
+});
+
+export default app;
