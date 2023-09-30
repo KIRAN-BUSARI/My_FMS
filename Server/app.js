522ea6500efb4e4eee6ab40f640044be3759e24d
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-01 00:25:16 +0530

    Some Chnages in app.js

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,7 @@ config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
