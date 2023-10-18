948d8a49179da008a04715a8f6666503d077a041
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-18 21:40:28 +0530

    Added the Signu[ page

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <div className='h-screen flex items-center justify-center text-4xl bg-yellow-300 text-red-500'>Signup</div>
+    )
+}
