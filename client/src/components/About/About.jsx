29c5d9b0847a5f270276ba46228c0af2136d86c0
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-18 21:39:01 +0530

    Added the About page

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className='text-4xl text-red-500 bg-yellow-300 rounded-lg p-3'>About</h1>
+        </div>
+    )
+}
