bf2a4717a7776f5c12437de29dc29825f8678354
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-18 12:31:45 +0530

    Added the PageNotFound

---
+++ client/src/components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <h1 className='text-4xl text-orange-400 font-bold'>404 Page Not <span className=''>Found</span>🧐</h1>
+        </div>
+    )
+}
