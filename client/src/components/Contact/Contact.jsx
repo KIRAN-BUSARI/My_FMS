f7618dc49a23bdfcdf97322b12d4efbd8256efe5
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-18 21:39:30 +0530

    Added the Contact page

---
+++ client/src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div className='flex h-screen items-center justify-center'>
+            <h1 className="text-3xl text-orange-300 bg-slate-800">This is Contact Page</h1>
+        </div>
+    )
+}
