2e92b548b977bded5e9f562db9d8ef39ed15425b
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-18 21:40:08 +0530

    Added the Login page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className='h-screen flex items-center justify-center text-4xl bg-red-500 text-yellow-400'>Login</div>
+    )
+}
