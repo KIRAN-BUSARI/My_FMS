595ae23bc549b067a181e99f6135fa564d713793
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-10-27 19:51:03 +0530

    Added the PageNotFound Page

--- client/src/components/PageNotFound.jsx
+++ client/src/components/PageNotFound.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function PageNotFound() {
     return (
         <div className="h-screen flex items-center justify-center">
-            <h1 className='text-4xl text-orange-400 font-bold'>404 Page Not <span className=''>Found</span>🧐</h1>
+            <h1 className='text-6xl text-orange-400 font-bold'>404 Page Not <span className=''>Found</span>
+                <span className='translate-y-3 `animate-bounce transition-transform'>🧐</span></h1>
         </div>
     )
 }
